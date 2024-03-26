d991e8b96f5859d8f933d7871986da7b57d24a35
Teryn Morgan <80481046+terynmorgan@users.noreply.github.com>
2024-03-26 11:21:04 -0400

    Add files via upload

---
+++ Venn Analysis/generate_venn_counts.R
@@ -0,0 +1,119 @@
+# Install and load necessary packages
+install.packages("pacman")
+pacman::p_load(dplyr, stringr)
+
+### IMPORT DATA ### -----------------------------------------------------------
+# Datasets prior to DMAI deduplication
+DMAI_dat <- read.csv("DMAI-2021-2022-compiled-dedup.csv", encoding = "UTF-8")
+scopus_dat <- read.csv("Scopus/V4_results_20240126/Scopus_2021_2022_Results_20240126.csv")
+lens_dat <- read.csv("Lens/V2_results_20240123/Lens_2021_2022_Results_20240123.csv")
+aa_dat <- read.csv("Academic Analytics/Output Files/aa_fd_matches_articles_2021_2022.csv")
+wos_dat <- read.csv("WoS/V3_results_20240123/WoS_2021_2022_Results_20240123.csv")
+
+# Datasets after DMAI deduplication
+scopus_DMAI_dedup <- read.csv("DMAI Deduplication/V2_results_20240123/Scopus_notin_DMAI_20240123.csv")
+lens_DMAI_dedup <- read.csv("DMAI Deduplication/V2_results_20240123/Lens_notin_DMAI_20240123.csv")
+aa_DMAI_dedup <- read.csv("DMAI Deduplication/V2_results_20240123/Academic_Analytics_notin_DMAI_20240123.csv")
+wos_DMAI_dedup <- read.csv("DMAI Deduplication/V2_results_20240123/WoS_notin_DMAI_20240123.csv")
+OAP_dat <- read.csv("OAP Deduplication/OAP_Collection_Export_Processed.csv", na.strings=c("","NA"))
+
+
+### INNER JOINS CALC ### -------------------------------------------------------------
+# Performs inner join on DOI/Title to count duplicated records between datasets
+calc_common_records <- function(affil1_dat, affil1_DOI_col, affil1_title_col, affil2_dat, affil2_DOI_col, affil2_title_col){
+  
+  # Select rows from affil1_dat and affil2_dat where DOI = NA
+  affil1_NA_DOI <- subset(affil1_dat, is.na(affil1_dat[[affil1_DOI_col]]))
+  affil2_NA_DOI <- subset(affil2_dat, is.na(affil2_dat[[affil2_DOI_col]]))
+  
+  # Select rows from affil1_dat and affil2_dat where DOI != NA
+  affil1_non_NA_DOI <- subset(affil1_dat, !(is.na(affil1_dat[[affil1_DOI_col]])))
+  affil2_non_NA_DOI <- subset(affil2_dat, !(is.na(affil2_dat[[affil2_DOI_col]])))
+
+  # Performs inner join on DOI when DOI != NA
+  join_doi <- intersect(affil1_non_NA_DOI[[affil1_DOI_col]], affil2_non_NA_DOI[[affil2_DOI_col]])
+
+  # Skips title lowercase when comparing with DMAI dataset
+  if ((identical(affil1_dat, DMAI_dat)) | (identical(affil2_dat, DMAI_dat))) {
+    # Performs inner join on Title where DOI = NA
+    join_title <- intersect(affil1_NA_DOI[[affil1_title_col]], affil2_NA_DOI[[affil2_title_col]])
+    
+    # Returns the number of records from inner join on DOI/Title
+    return(length(join_doi) + length(join_title))
+  } else {
+    
+  # Convert title columns to lowercase
+  affil1_NA_DOI[[affil1_title_col]] <- tolower(affil1_NA_DOI[[affil1_title_col]])
+  affil2_NA_DOI[[affil2_title_col]] <- tolower(affil2_NA_DOI[[affil2_title_col]])
+  
+  # Performs inner join on Title where DOI = NA
+  join_title <- intersect(affil1_NA_DOI[[affil1_title_col]], affil2_NA_DOI[[affil2_title_col]])
+
+  # Returns the number of records from inner join on DOI/Title
+  return(length(join_doi) + length(join_title))
+  }
+}
+
+### INNER JOIN COUNT MATRICES ### --------------------------------------------------------------------
+
+# List of affiliations - allows for variable name extraction with names()
+# Data prior to DMAI Deduplication
+DMAI_lst <- list(scopus_dat = scopus_dat, lens_dat = lens_dat, wos_dat = wos_dat, aa_dat = aa_dat, DMAI_dat = DMAI_dat)
+# Data after DMAI Deduplication
+OAP_lst <- list(scopus_DMAI_dedup = scopus_DMAI_dedup, lens_DMAI_dedup = lens_DMAI_dedup, 
+                wos_DMAI_dedup = wos_DMAI_dedup, aa_DMAI_dedup = aa_DMAI_dedup, OAP_dat = OAP_dat)
+
+# List of DOI/Title column names for affiliations
+affil_cols <- list(
+  "Scopus" = c("DOI", "Title"), 
+  "Lens" = c("DOI", "Title"), 
+  "WoS" = c("DOI", "Title"), 
+  "AA" = c("DOI", "Title")
+)
+
+
+# Fill matrix with the results of inner join On Title/DOI between each affil
+fill_affil_matrix <- function(affils_lst, affils_cols, DMAI_bool){
+  
+  # Add elements to affils_lst/cols if for DMAI or OAP iterations
+  if (DMAI_bool == TRUE){
+    affil_cols$DMAI <- c("doi", "title")
+  } else{
+    affil_cols$OAP <- c("dc.identifier.doi", "dc.title")
+  }
+  
+  # Initialize 5*5 matrix of 0s
+  affil_mat <- matrix(rep(0, length(affils_lst)*length(affils_lst)), nrow = length(affils_lst), ncol = length(affils_lst))
+  
+  # Iterates row, col of affil_mat, filling each position with count of inner join records
+  for (i in 1:nrow(affil_mat)){
+    for(j in 1:ncol(affil_mat)){
+      # Extract DOI/Title column names at position i,j in affil_cols
+      affil_i_cols <- affil_cols[[i]]
+      affil_j_cols <- affil_cols[[j]]
+      
+      # Calculate inner join for affiliations at position i,j 
+      affil_inner_join <- calc_common_records(affils_lst[i][[names(affils_lst)[i]]], affil_i_cols[1], affil_i_cols[2], 
+                                              affils_lst[j][[names(affils_lst)[j]]], affil_j_cols[1], affil_j_cols[2])
+      # Update matrix with affil_inner_join 
+      affil_mat[i,j] = affil_inner_join
+    }
+  }
+  # Add row and column names to matrix
+  colnames(affil_mat) <- names(affil_cols)
+  rownames(affil_mat) <- names(affil_cols)
+  
+  return(affil_mat)
+}
+
+
+# Retrieve inner join counts prior to DMAI deduplication
+DMAI_mat <- fill_affil_matrix(DMAI_lst, affil_cols, TRUE)
+
+# Retrieve inner join counts after DMAI deduplication and prior to OAP deduplication
+OAP_mat <- fill_affil_matrix(OAP_lst, affil_cols, FALSE)
+
+### EXPORT DATA ### -------------------------------------------------------------------
+# Export count matrices as csv files
+write.csv(DMAI_mat, "Venn Analysis/Output Count Files/Venn_Counts_Before_DMAI_Dedup.csv", row.names=TRUE)
+write.csv(OAP_mat, "Venn Analysis/Output Count Files/Venn_Counts_Before_OAP_Dedup.csv", row.names=TRUE)
---
+++ Venn Analysis/venn_diagram_DMAI_analysis.R
@@ -0,0 +1,120 @@
+# Install and load necessary packages
+install.packages("pacman")
+pacman::p_load(ggvenn, venn, ggplot2, ggpolypath)
+
+### IMPORT DATA ### -----------------------------------------------------------
+DMAI_dat <- read.csv("DMAI-2021-2022-compiled-dedup.csv")
+scopus_dat <- read.csv("Scopus/V4_results_20240126/Scopus_2021_2022_Results_20240126.csv")
+lens_dat <- read.csv("Lens/V2_results_20240123/Lens_2021_2022_Results_20240123.csv")
+aa_affil_dat <- read.csv("Academic Analytics/aa_fd_matches_articles_2021_2022.csv")
+wos_dat <- read.csv("WoS/V3_results_20240123/WoS_2021_2022_Results_20240123.csv")
+
+### SEPARATE DATA BASED ON DOI ### --------------------------------------------
+# Separate each df by records where DOI = NA
+separate_NA <- function(affil_dat, doi_col){
+  
+  # Select frows from affil_dat where DOI = NA
+  affil_NA_DOI <- subset(affil_dat, is.na(doi_col))
+  # affil_dat DOI != NA
+  affil_non_NA_DOI <- subset(affil_dat, !(is.na(doi_col)))
+  
+  # Remove duplicate rows based on DOI
+  affil_non_NA_DOI <- affil_non_NA_DOI[!duplicated(doi_col),]
+  
+  # Returns list of dataframes: DOI = NA, DOI != NA
+  return(list(affil_NA_DOI, affil_non_NA_DOI))
+}
+
+# Create list of 2 dataframes: where DOI = NA and DOI != NA
+DMAI_sep_list <- separate_NA(DMAI_dat, DMAI_dat[["doi"]]) # N = (98, 2852)
+scopus_sep_list <- separate_NA(scopus_dat, scopus_dat[["DOI"]]) # N = (0, 8359)
+lens_sep_list <- separate_NA(lens_dat, lens_dat[["DOI"]]) # N = (168, 5619)
+aa_affil_sep_list <- separate_NA(aa_affil_dat, aa_affil_dat[["DOI"]]) # N = (0, 8921)
+wos_sep_list <- separate_NA(wos_dat, wos_dat[["DOI"]] ) # N = (73,2946)
+
+### CREATE DOI/TITLE LISTS ### -------------------------------------------------
+
+# Title list for venn comparison
+comb_title <- list(
+  DMAI = DMAI_dat$title,
+  Scopus = scopus_dat$Title, 
+  Lens = lens_dat$Title, 
+  AA = aa_affil_dat$Title,
+  WoS = wos_dat$Title
+)
+
+# DOI list where DOI != NA for venn comparison 
+comb_DOI <- list(
+  DMAI = DMAI_sep_list[[2]]$doi,
+  Scopus = scopus_sep_list[[2]]$DOI,
+  Lens = lens_sep_list[[2]]$DOI,
+  AA = aa_affil_sep_list[[2]]$DOI,
+  WoS = wos_sep_list[[2]]$DOI
+)
+
+### GGVENN VISUALIZATIONS ### -------------------------------------------------------
+# ggvenn package can only handle 4 sets 
+
+# Venn Diagram between affiliation DOIs where DOI != NA
+png("Venn Analysis/Affiliation_DOI_Venn_Diagram.png")
+venn_plot <- ggvenn(
+  comb_DOI[2:length(comb_DOI)], 
+  fill_color = c("#0073C2FF", "#EFC000FF", "#868686FF", "#CD534CFF"),
+  stroke_size = 0.5, set_name_size = 4
+)
+venn_plot <- venn_plot + 
+                labs(title ="Affiliation DOI Venn Diagram",
+                    subtitle = "Plot of DOI when DOI is not missing") + 
+                theme(plot.title = element_text(face="bold", hjust = 0.5), 
+                      plot.subtitle = element_text(face = "italic", hjust = 0.5))
+print(venn_plot)
+dev.off()
+
+
+# Venn Diagram between affiliation Titles without filtration
+png("Venn Analysis/Affiliation_Title_Venn_Diagram.png")
+venn_plot <- ggvenn(
+  comb_title[2:length(comb_title)], 
+  fill_color = c("#0073C2FF", "#EFC000FF", "#868686FF", "#CD534CFF"),
+  stroke_size = 0.5, set_name_size = 4
+)
+venn_plot <- venn_plot + labs(title = "Affiliation Title Venn Diagram") + theme(plot.title = element_text(face="bold", hjust = 0.5))
+print(venn_plot)
+dev.off()
+
+
+### VENN PACKAGE VISUALIZATIONS ### ----------------------------------------------------------------------
+# To show intersection of DMAI, Scopus, Lens, AA, and Wos - need intersection of 5 sets
+
+# Venn Diagram between affiliation DOIs where DOI != NA
+png("Venn Analysis/DMAI_Affiliation_DOI_Venn_Diagram.png")
+venn_plot <- venn(
+  comb_DOI, 
+  ilabels = "counts", 
+  zcolor = "#911eb4, #F2CA19, #e6194B, #0057E9, #3cb44b", 
+  ggplot = TRUE
+)
+venn_plot <- venn_plot + 
+                labs(title ="Affiliation DOI Venn Diagram",
+                     subtitle = "Plot of DOI when DOI is not missing") + 
+                theme(plot.title = element_text(face="bold", hjust = 0.5, size = 16, margin = margin(b = 2)), 
+                      plot.subtitle = element_text(face = "italic", hjust = 0.5, size = 12, margin = margin(b = -15))) 
+print(venn_plot)
+dev.off()
+
+# Venn Diagram between affiliation Titles
+png("Venn Analysis/DMAI_Affiliation_Title_Venn_Diagram.png")
+venn_plot <- venn(
+  comb_title, 
+  ilabels = "counts", 
+  zcolor = "#911eb4, #F2CA19, #e6194B, #0057E9, #3cb44b", 
+  ggplot = TRUE
+)
+venn_plot <- venn_plot +
+                labs(title = "Affiliation Title Venn Diagram") + 
+                theme(plot.title = element_text(face="bold", hjust = 0.5, size = 16, margin = margin(b = -15)))
+print(venn_plot)
+dev.off()  
+
+
+
